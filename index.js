f82f6e599c9b4ca0cd374086474201543465cb2a
Javier Vidal <jvpenya@gmail.com>
2018-09-01 17:52:06 +0200

    Add more endpoints

--- index.js
+++ index.js
@@ -6,19 +6,53 @@ const of = require("rxjs/observable/of").of;
 const hostname = '127.0.0.1';
 const port = 1337;
 
-const $requests = new Subject();
+const routes = {};
 
-const endpoint = $requests =>
+addEndPoint = (url, epic) => {
+	const requests$ = new Subject();
+	epic(requests$).subscribe(({req, res}) => {
+		res.end();
+	});
+	routes[url] = requests$;
+}
+
+addEndPoint("/", $requests =>
 	$requests.pipe(
-		tap(({req, res}) => res.write("hola")),
-	);
+		tap(({req, res}) => res.write("welcome")),
+	)
+);
 
-endpoint($requests).subscribe(({req, res}) => {
-	res.end();
-});
+
+addEndPoint("/purchase", $requests =>
+	$requests.pipe(
+		tap(({req, res}) => res.write("purchase")),
+	)
+);
+
+addEndPoint("/methods", $requests =>
+	$requests.pipe(
+		tap(({req, res}) => res.write("methods")),
+	)
+);
+
+
+addEndPoint("/users", $requests =>
+	$requests.pipe(
+		tap(({req, res}) => {
+			ajax("https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc")
+			res.write("create")
+		}),
+	)
+);
+
+
 
 http.createServer((req, res) => {
-	$requests.next({ req: req, res: res });
+	if (routes.hasOwnProperty(req.url)) {
+		routes[req.url].next({req: req, res: res});
+	}
+
+	//$requests.next({req: req, res: res});
 	console.log(req.url);
 }).listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
