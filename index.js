18be63fa2234cd89a33bca594e56881c8ef9cd72
Javier Vidal <jvpenya@gmail.com>
2018-09-02 12:19:49 +0200

    Add prettier. Add more complicated examples

--- index.js
+++ index.js
@@ -1,59 +1,60 @@
-const http = require('http');
+const http = require("http");
 const Subject = require("rxjs/internal/Subject").Subject;
 const tap = require("rxjs/operators/tap").tap;
-const map = require("rxjs/operators/map").map;
-const of = require("rxjs/observable/of").of;
-const hostname = '127.0.0.1';
+const mergeMap = require("rxjs/operators/mergeMap").mergeMap;
+const mapTo = require("rxjs/operators/mapTo").mapTo;
+const request = require("universal-rxjs-ajax").request;
+
+const hostname = "127.0.0.1";
 const port = 1337;
 
 const routes = {};
 
 addEndPoint = (url, epic) => {
-	const requests$ = new Subject();
-	epic(requests$).subscribe(({req, res}) => {
-		res.end();
-	});
-	routes[url] = requests$;
-}
+  const requests$ = new Subject();
+  epic(requests$).subscribe(({ req, res }) => {
+    res.end();
+  });
+  routes[url] = requests$;
+};
 
 addEndPoint("/", $requests =>
-	$requests.pipe(
-		tap(({req, res}) => res.write("welcome")),
-	)
+  $requests.pipe(tap(({ req, res }) => res.write("welcome")))
 );
 
-
 addEndPoint("/purchase", $requests =>
-	$requests.pipe(
-		tap(({req, res}) => res.write("purchase")),
-	)
+  $requests.pipe(tap(({ req, res }) => res.write("purchase")))
 );
 
 addEndPoint("/methods", $requests =>
-	$requests.pipe(
-		tap(({req, res}) => res.write("methods")),
-	)
+  $requests.pipe(tap(({ req, res }) => res.write("methods")))
 );
-
 
 addEndPoint("/users", $requests =>
-	$requests.pipe(
-		tap(({req, res}) => {
-			ajax("https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc")
-			res.write("create")
-		}),
-	)
+  $requests.pipe(
+    mergeMap(({ req, res }) =>
+      request({
+        url: `https://api.github.com/search/users?q=javidalpe`
+      }).pipe(
+        tap(response => res.write(JSON.stringify(response.response.items))),
+        mapTo({ req, res })
+      )
+    )
+  )
 );
 
+http
+  .createServer((req, res) => {
+    if (routes.hasOwnProperty(req.url)) {
+      routes[req.url].next({ req, res });
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
 
-
-http.createServer((req, res) => {
-	if (routes.hasOwnProperty(req.url)) {
-		routes[req.url].next({req: req, res: res});
-	}
-
-	//$requests.next({req: req, res: res});
-	console.log(req.url);
-}).listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+    // $requests.next({req: req, res: res});
+    console.log(req.url);
+  })
+  .listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
